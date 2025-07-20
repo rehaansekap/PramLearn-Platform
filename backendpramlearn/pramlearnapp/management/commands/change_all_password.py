831698f74336ef7560739c943c965d7d0bdbf057
Raihan Syeka Pramukastie <rehaansekap@gmail.com>
2025-07-21 05:16:09 +0700

    Modify Default Password Command

--- backendpramlearn/pramlearnapp/management/commands/change_all_password.py
+++ backendpramlearn/pramlearnapp/management/commands/change_all_password.py
@@ -1,8 +1,16 @@
+from django.core.management.base import BaseCommand
 from pramlearnapp.models import CustomUser
 from django.contrib.auth.hashers import make_password
 
-# Bulk update semua user sekaligus
-hashed_password = make_password("123")
-updated_count = CustomUser.objects.update(password=hashed_password)
 
-print(f"Password berhasil diubah untuk {updated_count} users")
+class Command(BaseCommand):
+    help = 'Change password for all users to "123"'
+
+    def handle(self, *args, **options):
+        # Bulk update semua user sekaligus
+        hashed_password = make_password("123")
+        updated_count = CustomUser.objects.update(password=hashed_password)
+
+        self.stdout.write(
+            self.style.SUCCESS(f"Password berhasil diubah untuk {updated_count} users")
+        )
